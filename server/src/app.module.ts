8c28b92ccaf94bb14ba80f09e9c5d55180ba7ae7
Lena Voytek <lena@voytek.dev>
2022-12-28 02:37:20 -0700

    Add server database ORM setup

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(
+    (process.env["DB_TYPE"] && process.env["DB_TYPE"] != "sqlite") ?
+    {
+      type: process.env["DB_TYPE"] as "mysql" | "mariadb" | "postgres" || "mysql",
+      database: process.env["DB_NAME"] || "spelltracker",
+      username: process.env["DB_USERNAME"] || "root",
+      password: process.env["DB_PASSWORD"] || "",
+      entities: [__dirname + '/**/*.entity.ts'],
+      synchronize: true,
+    } :
+    {
+      type: "sqlite",
+      database: process.env["DB_NAME"] || "../db.sqlite3",
+      entities: [__dirname + '/**/*.entity.ts'],
+      synchronize: true,
+    }
+    )],
   controllers: [AppController],
   providers: [AppService],
 })
