c56befa9265098e9495888c745492bd77c1d089e
Lena Voytek <lena@voytek.dev>
2022-12-27 23:28:04 -0700

    Add .env and database dependencies

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
+  const PORT: number = parseInt(process.env["PORT"] as string, 10) || 3000;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
