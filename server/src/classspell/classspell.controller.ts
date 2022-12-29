e0df323e5ab519bb35cb5e8d31920330dc40a789
Lena Voytek <lena@voytek.dev>
2022-12-29 01:00:20 -0700

    Get more relevant info from class-by-spell

--- server/src/classspell/classspell.controller.ts
+++ server/src/classspell/classspell.controller.ts
@@ -1,21 +1,50 @@
 import { Controller, Get, Post, Delete, Body, Param } from '@nestjs/common';
+
 import { ClassSpellService } from './classspell.service';
-import { PathfinderClassSpell } from 'src/entities/pathfinderclassspell.entity';
 import { ClassService } from 'src/class/class.service';
 import { SpellService } from 'src/spell/spell.service';
+
+import { PathfinderClassSpell } from 'src/entities/pathfinderclassspell.entity';
+import { PathfinderClass } from 'src/entities/pathfinderclass.entity';
+import { PathfinderSpell } from 'src/entities/pathfinderspell.entity';
+import e from 'express';
 
 @Controller('classspell')
 export class ClassSpellController {
   constructor(private classSpellService: ClassSpellService, private classService: ClassService, private spellService: SpellService){}
 
   @Get('byclass/:name')
-  async readByClass(@Param('name') name: string): Promise<PathfinderClassSpell[]> {
-    return this.classSpellService.getByClass(name);
+  async readByClass(@Param('name') name: string): Promise<PathfinderSpell[][]> {
+    const classSpells = await this.classSpellService.getByClass(name);
+    const maxSpellLevel = Math.max(...classSpells.map((classSpell) => classSpell.spell_level), 0);
+
+    let spells = Array<PathfinderSpell[]>(maxSpellLevel);
+
+    for(const classSpell of classSpells) {
+      if(!spells[classSpell.spell_level])
+        spells[classSpell.spell_level] = [classSpell.spell];
+      else
+        spells[classSpell.spell_level].push(classSpell.spell);
+    }
+
+    return spells;
   }
 
   @Get('byspell/:name')
-  async readBySpell(@Param('name') name: string): Promise<PathfinderClassSpell[]> {
-    return this.classSpellService.getBySpell(name);
+  async readBySpell(@Param('name') name: string): Promise<PathfinderClass[][]> {
+    const classSpells = await this.classSpellService.getBySpell(name);
+    const maxSpellLevel = Math.max(...classSpells.map((classSpell) => classSpell.spell_level), 0);
+
+    let classes = Array<PathfinderClass[]>(maxSpellLevel);
+
+    for(const classSpell of classSpells) {
+      if(!classes[classSpell.spell_level])
+        classes[classSpell.spell_level] = [classSpell.spellcaster_class];
+      else
+        classes[classSpell.spell_level].push(classSpell.spellcaster_class);
+    }
+
+    return classes;
   }
 
   @Post('add')
@@ -25,7 +54,10 @@ export class ClassSpellController {
     newClassSpell.spell = await this.spellService.get(info.spellName);
     newClassSpell.spell_level = info.spellLevel;
 
-    return this.classSpellService.create(newClassSpell);
+    if(newClassSpell.spellcaster_class && newClassSpell.spell)
+      return this.classSpellService.create(newClassSpell);
+    else
+      return {"error": "Invalid spell/class input"};
   }
 
   @Delete('remove')
--- server/src/classspell/classspell.service.ts
+++ server/src/classspell/classspell.service.ts
@@ -15,11 +15,11 @@ export class ClassSpellService {
   }
 
   async getByClass(className: string): Promise<PathfinderClassSpell[]> {
-    return await this.classSpellRepository.find({where: {spellcaster_class: {name: className}}});
+    return await this.classSpellRepository.createQueryBuilder("pathfinderclassspell").leftJoinAndSelect("pathfinderclassspell.spell", "spell").leftJoinAndSelect("pathfinderclassspell.spellcaster_class", "class").where("class.name = :classname", {classname: className}).getMany();
   }
 
   async getBySpell(spellName: string): Promise<PathfinderClassSpell[]> {
-    return await this.classSpellRepository.find({where: {spell: {name: spellName}}});
+    return await this.classSpellRepository.createQueryBuilder("pathfinderclassspell").leftJoinAndSelect("pathfinderclassspell.spell", "spell").leftJoinAndSelect("pathfinderclassspell.spellcaster_class", "class").where("spell.name = :spellname", {spellname: spellName}).getMany();
   }
 
   async delete(className: string, spellName: string): Promise<DeleteResult> {
