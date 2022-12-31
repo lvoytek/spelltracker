7e400d753b5c9ac8746909ed69b7a0c3e4d50596
Lena Voytek <lena@voytek.dev>
2022-12-31 01:47:16 -0700

    Add JwtAuthGuard

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,10 +1,18 @@
-import { Controller, Put, Delete, Body, Param } from '@nestjs/common';
+import { Controller, Get, Put, Delete, Request, Body, Param, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from 'src/entities/user.entity';
+import { JwtAuthGuard } from 'src/auth/jwt.guard';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService){}
+
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  async read(@Request() req): Promise<User> {
+    return this.userService.get(req.user.email);
+  }
 
   @Put()
   async update(@Body() user: User): Promise<any> {
